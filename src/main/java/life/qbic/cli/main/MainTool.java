056d609f007a6a198777b597fca9286398703af4
Julian Späth <spaethju@posteo.de>
2018-08-15 13:01:25 +0200

    Port to new template

--- src/main/java/life/qbic/cli/main/MainTool.java
+++ src/main/java/life/qbic/cli/main/MainTool.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import life.qbic.cli.QBiCTool;
+import life.qbic.cli.connection.CredentialHandler;
 import life.qbic.cli.helper.GEOExcelCreater;
 import life.qbic.cli.helper.GEOOpenBisParser;
 import org.apache.logging.log4j.LogManager;
@@ -33,39 +34,54 @@ public class MainTool extends QBiCTool<MainCommand> {
   public void execute() {
     // get the parsed command-line arguments
     final MainCommand command = super.getCommand();
+    LOG.info("Parse commands");
 
-    java.io.Console console = System.console();
-    String password = new String(console.readPassword("Password: "));
-    if (password.isEmpty()) {
-      System.out.println("You need to provide a password.");
+    String password = "";
+
+    IDataStoreServerApi dss = null;
+    IApplicationServerApi app = null;
+
+    // Connect to openBis
+
+    try {
+      // Reference the DSS
+       dss =
+          HttpInvokerUtils.createStreamSupportingServiceStub(IDataStoreServerApi.class,
+              "https://qbis.qbic.uni-tuebingen.de:444/datastore_server"
+                  + IDataStoreServerApi.SERVICE_URL, 10000);
+
+      // get a reference to AS API
+       app = HttpInvokerUtils.createServiceStub(IApplicationServerApi.class,
+          "https://qbis.qbic.uni-tuebingen.de/openbis/openbis" + IApplicationServerApi.SERVICE_URL,
+          10000);
+       LOG.info("openBis connection established");
+    } catch (Exception e) {
+      LOG.error("Could not connect to openBis");
       System.exit(1);
     }
 
-    // Reference the DSS
-    IDataStoreServerApi dss =
-        HttpInvokerUtils.createStreamSupportingServiceStub(IDataStoreServerApi.class,
-            "https://qbis.qbic.uni-tuebingen.de:444/datastore_server"
-                + IDataStoreServerApi.SERVICE_URL, 10000);
-
-    // get a reference to AS API
-    IApplicationServerApi app = HttpInvokerUtils.createServiceStub(IApplicationServerApi.class,
-        "https://qbis.qbic.uni-tuebingen.de/openbis/openbis" + IApplicationServerApi.SERVICE_URL,
-        10000);
+    try {
+      java.io.Console console = System.console();
+      password = new String(console.readPassword("Password: "));
+      if (password.isEmpty()) {
+        System.out.println("You need to provide a password");
+        LOG.error("No password provided");
+        System.exit(1);
+      }
+    }catch (NullPointerException e) {
+      CredentialHandler ch = new CredentialHandler("/Users/spaethju/qbic-ext.properties");
+      password = ch.getPw();
+    }
 
     // login to obtain a session token
-    String sessionToken = app.login(command.userName, password);
-
-    System.out.println("Connection to openbis established succesfully.");
-
-    System.out.format("Provide project identifier:  ");
-
-    if (command.project.isEmpty() || command.project == null) {
-      System.out.println("You need to provide a project.");
-      System.exit(1);
+    String sessionToken = "";
+    try {
+      sessionToken = app.login(command.userName, password);
+      LOG.info("Logged in successfully to openBis");
+    } catch (Exception e) {
+      LOG.error("Could not log in to openBis. Please check your username and password");
     }
-    String[] split = command.project.split("/");
-    command.project = split[split.length - 1];
-    System.out.println(command.project);
+
     // Parse space
     GEOOpenBisParser geoParser = new GEOOpenBisParser(command.project, command.userName,
         sessionToken,
